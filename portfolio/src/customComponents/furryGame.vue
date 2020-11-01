adaa5a0b8ccb6e3083097a35be7b42f2b4ac11e6
Zbyszek <it3@incpraca.pl>
2020-11-01 21:43:10 +0100

    game engine ready

--- portfolio/src/customComponents/furryGame.vue
+++ portfolio/src/customComponents/furryGame.vue
@@ -7,6 +7,9 @@
         :key="index"
       ></div>
     </div>
+    <div class="text-center score">
+      <h1>Wynik: {{ this.score }}</h1>
+    </div>
   </div>
 </template>
 
@@ -25,10 +28,15 @@ export default {
       furryPosition: {
         x: 0,
         y: 0,
+        direction: "right",
       },
+      score: 0,
     };
   },
   mounted() {
+    window.addEventListener("keydown", (e) => {
+      this.changeDirection(e.keyCode);
+    });
     this.insertFurry();
     this.insertCoin();
     this.moveFurry();
@@ -50,20 +58,83 @@ export default {
     },
     coinPositionX() {
       this.coinPosition.x = Math.floor(Math.random() * 10);
-      return Math.floor(Math.random() * 10);
+      return this.coinPosition.x;
     },
     coinPositionY() {
       this.coinPosition.y = Math.floor(Math.random() * 10);
-      return Math.floor(Math.random() * 10);
+      return this.coinPosition.y;
     },
     divIndex(x, y) {
-      return x + y * 10;
+      return x + y * 12;
     },
     moveFurry() {
       this.intervalFun = setInterval(() => {
-        this.furryPosition.x = this.furryPosition.x + 1;
+        $("#board")
+          .find("div")
+          [
+            this.divIndex(this.furryPosition.x, this.furryPosition.y)
+          ].classList.remove("furry");
+        switch (this.furryPosition.direction) {
+          case "right":
+            this.furryPosition.x = this.furryPosition.x + 1;
+            break;
+          case "left":
+            this.furryPosition.x = this.furryPosition.x - 1;
+            break;
+          case "up":
+            this.furryPosition.y = this.furryPosition.y - 1;
+            break;
+          case "down":
+            this.furryPosition.y = this.furryPosition.y + 1;
+            break;
+        }
+        this.checkCoinCollision();
+        this.checkWallCollision();
         this.insertFurry();
       }, 250);
+    },
+    checkWallCollision() {
+      console.log("y: ", this.furryPosition.y, "x: ", this.furryPosition.x);
+      if (
+        this.furryPosition.x > 11 ||
+        this.furryPosition.x < 0 ||
+        this.furryPosition.y > 11 ||
+        this.furryPosition.y < 0
+      ) {
+        alert("game over");
+        clearInterval(this.intervalFun);
+      }
+    },
+    checkCoinCollision() {
+      if (
+        this.furryPosition.x === this.coinPosition.x &&
+        this.furryPosition.y === this.coinPosition.y
+      ) {
+        $("#board")
+          .find("div")
+          [
+            this.divIndex(this.coinPosition.x, this.coinPosition.y)
+          ].classList.remove("coin");
+        this.insertCoin();
+        this.score += 1;
+      }
+    },
+    changeDirection(e) {
+      console.log(e);
+      switch (e) {
+        case 40:
+          this.furryPosition.direction = "down";
+          break;
+        case 38:
+          this.furryPosition.direction = "up";
+          break;
+        case 37:
+          this.furryPosition.direction = "left";
+          break;
+        case 39:
+          this.furryPosition.direction = "right";
+          break;
+      }
     },
   },
   computed: {},
@@ -99,5 +170,8 @@ export default {
   .cell {
     height: 64px;
   }
+}
+.score {
+  color: green;
 }
 </style>
