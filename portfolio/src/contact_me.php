ce724de751a088298d0bb5343c6cee48e9643d57
Zbyszek <it3@incpraca.pl>
2021-01-17 21:16:18 +0100

    emial validation in php

--- portfolio/src/contact_me.php
+++ portfolio/src/contact_me.php
@@ -2,7 +2,8 @@
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
-   empty($_POST['message']))
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
    return false;
