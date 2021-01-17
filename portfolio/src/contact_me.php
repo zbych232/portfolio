0b1e5044d902f019eb9292ab527bceee19519eda
Zbyszek <it3@incpraca.pl>
2021-01-17 21:20:44 +0100

    form sending ok. polish message

--- portfolio/src/contact_me.php
+++ portfolio/src/contact_me.php
@@ -16,8 +16,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'stolarskizbigniew93@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Formularz zbigniewstolarski.pl:  $name";
+$email_body = "Wiadomość ze strony zbigniewstolarski.pl\n\n"."Treść:\n\nImię: $name\n\nEmail: $email_address\n\nWiadomość:\n$message";
 $headers = "From: stolarskizbigniew93@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
