2e1dca1ad085b1cadbe6bc2ded2f061dfcaaa924
it3incpraca <it3@incpraca.pl>
2021-03-07 13:08:49 +0100

    scroll to about me section

--- portfolio/src/views/mainPage.vue
+++ portfolio/src/views/mainPage.vue
@@ -12,7 +12,7 @@
         </div>
       </div>
     </div>
-    <div class="section">
+    <div class="section" id="aboutMe" style="padding:0">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
           fill="#5000ca"
